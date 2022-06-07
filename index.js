61fa6c32c0506d334872db6d30f706a44c251b71
northkuria <kuri4martin@gmail.com>
2022-06-07 15:17:29 +0300

    added monday and saturday activity

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (adj = '*') => {
+  return (param = 'special') => `You are ${adj}${param}${adj}!`
+}
